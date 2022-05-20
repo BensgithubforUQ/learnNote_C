1f95fe4ac7f685d4d15e1e62200b1fbb72648306
BensgithubforUQ <393114076@qq.com>
2022-05-20 21:57:23 +0800

    增加了一些注释

--- cplusplusCoding/dynamicLibrary/main.c
+++ cplusplusCoding/dynamicLibrary/main.c
@@ -2,7 +2,8 @@
 #include "head.h"
 
 int main()//g++ main.c -o app -I include/ -L lib/ -l calc 编译方法
-{
+{   //动态库在进程需要用到动态库中的方法的时候，才会加载，因此节约内存，并且易于更新发布和部署
+    //因此加载速度比静态库稍微慢一点，发布程序的时候，需要提供依赖的动态库。
     //g++ -c -fpic add.c div.c mult.c sub.c -I ../include/ 无链接的对象文件生成
     //g++ -shared add.o div.o mult.o sub.o -o libcalc.so 动态库生成
     //直接编译得到的app文件不可执行，找不到动态库，
@@ -13,9 +14,12 @@ int main()//g++ main.c -o app -I include/ -L lib/ -l calc 编译方法
     //这样可以让app找到动态库，配置命令如下（临时，这个关掉终端就又不认了）
     //export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ben/Linux/learnNote_C/cplusplusCoding/dynamicLibrary/lib
     //所以用别的方法
-    //用户级别的配置
+    //用户级别的配置：
     //用cd进入home目录，找环境变量配置文件.bashrc
     //在文件末尾加入上述的环境变量配置，就可以运行（记得 source .bashrc）
+    //系统级别的配置方法：
+    //在/etc/profile中加入环境变量的配置
+    
     int a = 20;
     int b = 12;
     printf("a = %d, b = %d\n", a, b);
