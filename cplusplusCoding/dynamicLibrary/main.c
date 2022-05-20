f8be8021c29dd3a52c81d1bae74d5f0f2d1e054b
BensgithubforUQ <393114076@qq.com>
2022-05-20 21:50:32 +0800

    动态库和动态库的环境变量搭建问题

---
+++ cplusplusCoding/dynamicLibrary/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "head.h"
+
+int main()//g++ main.c -o app -I include/ -L lib/ -l calc 编译方法
+{
+    //g++ -c -fpic add.c div.c mult.c sub.c -I ../include/ 无链接的对象文件生成
+    //g++ -shared add.o div.o mult.o sub.o -o libcalc.so 动态库生成
+    //直接编译得到的app文件不可执行，找不到动态库，
+    //所以这里在app文件执行的时候，链接阶段找不到动态库。
+    //用ldd app来查询依赖库。
+    //这时候，就需要用系统的动态加载器来获取动态库的绝对路径。
+    //可以通过配置环境变量，将该动态库加入到环境变量中，让动态加载起获取绝对路径的方式
+    //这样可以让app找到动态库，配置命令如下（临时，这个关掉终端就又不认了）
+    //export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ben/Linux/learnNote_C/cplusplusCoding/dynamicLibrary/lib
+    //所以用别的方法
+    //用户级别的配置
+    //用cd进入home目录，找环境变量配置文件.bashrc
+    //在文件末尾加入上述的环境变量配置，就可以运行（记得 source .bashrc）
+    int a = 20;
+    int b = 12;
+    printf("a = %d, b = %d\n", a, b);
+    printf("a + b = %d\n", add(a, b));
+    printf("a - b = %d\n", subtract(a, b));
+    printf("a * b = %d\n", multiply(a, b));
+    printf("a / b = %f\n", divide(a, b));
+    return 0;
+}
