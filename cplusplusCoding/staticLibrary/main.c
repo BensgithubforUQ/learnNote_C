4153e20d6575183e76590f88b47b359f357b84b0
“BensgithubforUQ” <393110476@qq.com>
2022-05-20 20:40:43 +0800

    update comment for 静态库

--- cplusplusCoding/staticLibrary/main.c
+++ cplusplusCoding/staticLibrary/main.c
@@ -3,8 +3,20 @@
 
 int main()
 {
+//     Linux : libxxx.a
+// lib : 前缀（固定）
+// xxx : 库的名字，自己起
+// .a : 后缀（固定）
     int a = 20;
     int b = 12;
+// Windows : libxxx.lib
+// 静态库的制作：
+// ◆ gcc 获得 .o 文件
+// ◆ 将 .o 文件打包，使用 ar 工具（archive）
+// ar rcs libxxx.a xxx.o xxx.o
+// r – 将文件插入备存文件中
+// c – 建立备存文件
+// s – 索引
     printf("a = %d, b = %d\n", a, b);
     printf("a + b = %d\n", add(a, b));
     printf("a - b = %d\n", subtract(a, b));
